297cfc8859027378c46aa231ae3b5e113a302ef9
tpb17 <163563422+tpb17@users.noreply.github.com>
2024-05-26 15:47:08 -0500

    finished website

--- javascript.js
+++ javascript.js
@@ -1,12 +1,46 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
-        row.appendChild(box);
+function newGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        container.appendChild(column);
+        for (let j = 0; j < gridSize; j++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            column.appendChild(box);
+            
+            box.style.padding = `${400/gridSize}px`;
+    
+    
+            box.addEventListener("mouseover", () => {
+                box.style.backgroundColor = randomRGB();
+            })
+    
+        }
     }
 }
+
+function randomRGB() {
+    const num1 = (Math.floor(Math.random() * 255) + 1);
+    const num2 = (Math.floor(Math.random() * 255) + 1);
+    const num3 = (Math.floor(Math.random() * 255) + 1);
+
+    return `rgb(${num1}, ${num2}, ${num3})`;
+}
+
+newGrid(16);
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+    let inputSize = 101;
+    while (inputSize > 100 || inputSize == null || inputSize == "") {
+        inputSize = prompt("Enter the size of the grid:\n(Cannot be greater than 100)");
+    }
+    const columns = document.getElementsByClassName("column");
+    while (columns.length > 0) {
+        columns[0].parentNode.removeChild(columns[0]);
+    }
+
+    newGrid(inputSize);
+})
